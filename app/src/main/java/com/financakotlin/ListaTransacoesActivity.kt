ca8f46f297bf942b0f25be96663f9f1caf1dca8d
gucaveira <pereiragustavo215@gmail.com>
2019-11-01 20:33:41 -0300

    Initial commit

---
+++ app/src/main/java/com/financakotlin/ListaTransacoesActivity.kt
@@ -0,0 +1,12 @@
+package com.financakotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
