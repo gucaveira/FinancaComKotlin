b916e602ed1ed0b3b9911797d354a7dcea5eb525
gucaveira <pereiragustavo215@gmail.com>
2019-11-24 13:40:09 -0300

    Utilizando o Object Expression

--- app/src/main/java/com/financakotlin/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/financakotlin/ui/activity/ListaTransacoesActivity.kt
@@ -1,6 +1,8 @@
 package com.financakotlin.ui.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.financakotlin.R
 import com.financakotlin.model.Tipo
